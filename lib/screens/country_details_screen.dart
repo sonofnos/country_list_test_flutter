7b162ff8696e83a22a6fe80d4d0fd277bedc27ad
Christian Emeka <54124037+sonofnos@users.noreply.github.com>
2025-02-28 19:59:44 +0100

    Add CountryDetailsScreen widget with placeholder implementation

---
+++ lib/screens/country_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CountryDetailsScreen extends StatelessWidget {
+  const CountryDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
